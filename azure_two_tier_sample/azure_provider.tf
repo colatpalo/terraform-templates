aaa2071d7ed41d709d9f7df0434f5e4a6994129d
colatpalo <71885206+colatpalo@users.noreply.github.com>
2020-09-25 14:52:07 +0100

    Delete azure_provider.tf

--- azure_two_tier_sample/azure_provider.tf
+++
@@ -1,6 +0,0 @@
-provider "azurerm" {
-    subscription_id = "9a3eb5f8-75bc-48c1-b7c5-238802a9842d"
-    client_id = "ec25252a-a1e0-44b1-86e6-7c40d7e54116"
-    client_secret = "M0Gcl9Wl6yP1-Zi9~xeYhT~G8uM0.nOeEh"
-    tenant_id = "66b66353-3b76-4e41-9dc3-fee328bd400e"
-}
